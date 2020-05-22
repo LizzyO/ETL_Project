0f37a456dd76929ac2624eed6b2d98ad735ee5e1
David <40743697+metwoboy@users.noreply.github.com>
2020-05-21 21:40:57 -0700

    project udpates

--- ETL_Project.sql
+++ ETL_Project.sql
@@ -1,16 +1,16 @@
 -- Create tables for raw data to be loaded into
 CREATE TABLE imdb_title  (
-imdb_title_id INT PRIMARY KEY,
+imdb_title_id VARCHAR PRIMARY KEY,
 title TEXT
 );
 
 CREATE TABLE imdb_ratings (
-imdb_title_id INT PRIMARY KEY,
+imdb_title_id VARCHAR PRIMARY KEY,
 weighted_average_vote FLOAT
 );
 
 CREATE TABLE disney (
-imdb_title_id INT PRIMARY KEY,
+imdb_title_id VARCHAR PRIMARY KEY,
 title TEXT,
 imdb_rating FLOAT,
 plot TEXT,
@@ -20,8 +20,8 @@ added_at DATE
 );
 
 CREATE TABLE netflix (
-imdb_title_id INT PRIMARY KEY,
-title TEXT,
+show_id INT,
+title TEXT PRIMARY KEY,
 description TEXT,
 type TEXT,
 year TEXT,
